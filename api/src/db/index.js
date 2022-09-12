9bc0b36435e212ab7637a6c7be4521e6b8a3fda3
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-09-12 10:24:14 -0300

    postgre uri

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -7,7 +7,7 @@ const {
 
 const realURL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_USER}`
 
-const sequelize = new Sequelize(`postgres://ipvsgpxb:YG-hPtAoI9DMUXkTyHgfG0AEeZgO1Nsa@heffalump.db.elephantsql.com/ipvsgpxb`, {
+const sequelize = new Sequelize(realURL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
