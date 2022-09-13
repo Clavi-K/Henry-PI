6b0fd1000615372059379ce3b5491dc4efbe2a1f
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-09-13 07:53:43 -0300

    component test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { Route, BrowserRouter } from "react-router-dom"
 import App from './App';
+
+import Card from "./components/Card/Card"
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("renders a correct card component", () => {
+
+  render(<BrowserRouter><Card id="test" background_image={"test"} name="Test" Genres={["Test1", "Test2"]} /></BrowserRouter>)
+  const nameElement = screen.getByText(/Test/i)
+  expect(nameElement).toBeInTheDocument()
+
+})
